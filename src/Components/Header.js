8457468b0a8987e700646653a62bae7e107ec414
rejwan-ahamed <rejwanahamed85@gmail.com>
2022-11-08 22:37:20 +0600

    firebase login system added

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Navbar } from "flowbite-react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../Context/MainContext";
 
 const Header = () => {
+  const { userLogout, user } = useContext(AuthContext);
+  const userLogoutButtonClicked = () => {
+    userLogout();
+  };
   return (
     <div>
       <div className="main-nav-section border-b">
-        <Navbar fluid={true} rounded={true} className="md:mx-20 lg:mx-20 xl:mx-40">
+        <Navbar
+          fluid={true}
+          rounded={true}
+          className="md:mx-20 lg:mx-20 xl:mx-40"
+        >
           <Navbar.Brand href="/">
             <img
               src="/assets/images/logo.svg"
@@ -25,12 +34,32 @@ const Header = () => {
               <Link to="/blog">Blogs</Link>
               <Link to="/reviews">My review</Link>
               <Link to="/add">Add service</Link>
-              <button
-                type="button"
-                className="text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 "
-              >
-                Log out
-              </button>
+              {user ? (
+                <button
+                  onClick={userLogoutButtonClicked}
+                  type="button"
+                  className="text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 "
+                >
+                  Log out
+                </button>
+              ) : (
+                <div className="button-group block lg:flex">
+                  <Link
+                    to="/login"
+                    type="button"
+                    className="text-white bg-orange-500 hover:bg-orange-600 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 "
+                  >
+                    Login
+                  </Link>
+                  <Link
+                    to="/register"
+                    type="button"
+                    className="text-white bg-orange-500 hover:bg-orange-600 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 "
+                  >
+                    Register
+                  </Link>
+                </div>
+              )}
             </div>
           </Navbar.Collapse>
         </Navbar>
