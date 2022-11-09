57e19bd5f3be28a87677e7d2464de37b18880d1d
rejwan-ahamed <rejwanahamed85@gmail.com>
2022-11-09 13:14:33 +0600

    time counter added

--- src/Components/AddService.js
+++ src/Components/AddService.js
@@ -1,14 +1,56 @@
-import React from "react";
+import React, { useContext } from "react";
+import toast from "react-hot-toast";
+import { AuthContext } from "../Context/MainContext";
 import UseTitle from "./Title";
+import moment from "moment";
 
 const AddService = () => {
-  UseTitle('Add a new service')
+  // get user time
+
+  const { user } = useContext(AuthContext);
+  UseTitle("Add a new service");
+  const fromSubmit = (e) => {
+    const getUserTime = moment().format("MMMM Do YYYY, h:mm:ss a");
+    e.preventDefault();
+    const from = e.target;
+    const name = from.name.value;
+    const image = from.image.value;
+    const price = from.price.value;
+    const description = from.description.value;
+    // console.log(name,image,price,description)
+
+    const productDetails = {
+      userName: `${user.displayName}`,
+      email: `${user.email}`,
+      name: `${name}`,
+      image: `${image}`,
+      price: `${price}`,
+      description: `${description}`,
+      time: `${getUserTime}`,
+    };
+    console.log(productDetails);
+
+    fetch("http://localhost:5000/products", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(productDetails),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.acknowledged === true) {
+          e.target.reset();
+          toast.success("Your service was added successfully");
+        }
+      });
+  };
   return (
     <div className="px-8 sm:px-40 pb-20 pt-10">
       <h1 className="font-general text-center text-3xl font-[600] py-10">
         Add your service.
       </h1>
-      <form>
+      <form onSubmit={fromSubmit}>
         <div class="grid gap-6 mb-6 md:grid-cols-2">
           <div>
             <label
@@ -18,10 +60,11 @@ const AddService = () => {
               Service name
             </label>
             <input
+              name="name"
               type="text"
               id="first_name"
               class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-orange-500 dark:focus:border-orange-500"
-              placeholder="John"
+              placeholder="service name"
               required=""
             />
           </div>
@@ -33,10 +76,11 @@ const AddService = () => {
               Image (URL)
             </label>
             <input
-              type="text"
+              name="image"
+              type="url"
               id="last_name"
               class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-orange-500 dark:focus:border-orange-500"
-              placeholder="Doe"
+              placeholder="google.com"
               required=""
             />
           </div>
@@ -48,10 +92,11 @@ const AddService = () => {
               Price
             </label>
             <input
-              type="text"
+              name="price"
+              type="number"
               id="company"
               class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-orange-500 dark:focus:border-orange-500"
-              placeholder="Flowbite"
+              placeholder="290"
               required=""
             />
           </div>
@@ -63,10 +108,11 @@ const AddService = () => {
           Description
         </label>
         <textarea
+          name="description"
           id="message"
           rows="4"
           class=" mb-8 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-orange-500 focus:border-orange-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-orange-500 dark:focus:border-orange-500"
-          placeholder="Your message..."
+          placeholder="product description"
         ></textarea>
 
         <button
